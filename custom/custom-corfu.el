4c305c5b920b81f49efde6492bd88c7b2d3dbb1a
Julian Konrath <julian.konrath@e-vo.at>
2025-09-09 17:41:22 +0200

    add corfu config

--- custom/custom-corfu.el
+++ custom/custom-corfu.el
@@ -1,11 +1,19 @@
-;; corfu
-;; corfu complete on tab
+;;; custome-corfu.el --- Customizations for corfu -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
 (use-package corfu
   :init
   (global-corfu-mode) ;; ✅ Ensures it's activated
   :config
-  ;; Custom keybindings
+  ;; enable corfu completion on tab
   (define-key corfu-map [tab] 'corfu-complete)
   (define-key corfu-map [?\t] 'corfu-complete)
+
+  ;; disable corfu complete on enter
   (define-key corfu-map [return] nil)
-  (define-key corfu-map [?\r] nil))
+  (define-key corfu-map [?\nil] r)
+  )
+
+
+;;; custom-corfu.el ends here
