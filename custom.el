7431bf77e7a9c474b53dc55ea6ccf38617a994d4
Julian Konrath <julian.konrath@gmail.com>
2025-09-14 22:21:54 +0200

    fix fonts

--- custom.el
+++ custom.el
@@ -27,60 +27,7 @@
 (when (file-readable-p (expand-file-name  "custom-local.el" user-emacs-directory))
   (load-file (expand-file-name  "custom-local.el" user-emacs-directory)))
 
-;; Fonts
-(defun centaur-setup-fonts ()
-  "Setup fonts."
-  (when (display-graphic-p)
-    ;; Set default font
-    (cl-loop for font in '("Cascadia Code" "Fira Code" "Jetbrains Mono"
-                           "SF Mono" "Hack" "Source Code Pro" "Menlo"
-                           "Monaco" "DejaVu Sans Mono" "Consolas")
-             when (font-installed-p font)
-             return (set-face-attribute 'default nil
-                                        :family font
-                                        :height (cond (sys/macp 130)
-                                                      (sys/win32p 110)
-                                                      (t my/centaur-basic-font-size))))
 
-    ;; Set mode-line font
-    ;; (cl-loop for font in '("Menlo" "SF Pro Display" "Helvetica")
-    ;;          when (font-installed-p font)
-    ;;          return (progn
-
-    ;;                   (set-face-attribute 'mode-line nil :family font :height 120)
-    ;;                   (when (facep 'mode-line-active)
-    ;;                     (set-face-attribute 'mode-line-active nil :family font :height 120))
-    ;;                   (set-face-attribute 'mode-line-inactive nil :family font :height 120)))
-
-    ;; Specify font for all unicode characters
-    (cl-loop for font in '("Segoe UI Symbol" "Symbola" "Symbol")
-             when (font-installed-p font)
-             return (if (< emacs-major-version 27)
-                        (set-fontset-font "fontset-default" 'unicode font nil 'prepend)
-                      (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
-
-    ;; Emoji
-    (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji" "Segoe UI Emoji")
-             when (font-installed-p font)
-             return (cond
-                     ((< emacs-major-version 27)
-                      (set-fontset-font "fontset-default" 'unicode font nil 'prepend))
-                     ((< emacs-major-version 28)
-                      (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend))
-                     (t
-                      (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend))))
-
-    ;; Specify font for Chinese characters
-    (cl-loop for font in '("LXGW Neo Xihei" "WenQuanYi Micro Hei Mono" "LXGW WenKai Screen"
-                           "LXGW WenKai Mono" "PingFang SC" "Microsoft Yahei UI" "Simhei")
-             when (font-installed-p font)
-             return (progn
-                      (setq face-font-rescale-alist `((,font . 1.3)))
-                      (set-fontset-font t 'han (font-spec :family font))))))
-
-(centaur-setup-fonts)
-(add-hook 'window-setup-hook #'centaur-setup-fonts)
-(add-hook 'server-after-make-frame-hook #'centaur-setup-fonts)
 
 ;; Mail
 ;; (setq message-send-mail-function 'smtpmail-send-it
@@ -163,9 +110,13 @@
 ;; font
 (set-frame-font "Hack Nerd Font-13" nil t)
 
+
 ;; ut8 encoding
 (prefer-coding-system 'utf-8)
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
+
+;; global key bindings
+(define-key global-map (kbd "C-x C-n") 'consult-buffer)
 
 ;;; custom.el ends here
