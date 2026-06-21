627091212af85b3c212e13f4528c54752bdfbc48
Julian Konrath <julian.konrath@gmail.com>
2026-06-21 15:44:51 +0200

    unbind right alt to use for umlauts

--- custom/custom-mac.el
+++ custom/custom-mac.el
@@ -7,4 +7,7 @@
 ;; (setq mac-right-option-modifier 'meta)
 (setq mac-option-modifier 'meta)
 
+(setq ns-alternate-modifier 'meta
+      ns-right-alternate-modifier 'none)
+
 ;;; custom-mac.el ends here
